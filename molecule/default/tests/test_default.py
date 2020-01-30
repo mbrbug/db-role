37ccee3e9ab03eb63b69545473bdb5f568601c17
Andrei Maiboroda <am@homembr.ru>
2020-01-30 21:32:41 +0300

    try other

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,6 +5,7 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
 # check if MongoDB is enabled and running
 def test_mongo_running_and_enabled(host):
     mongo = host.service("mongod")
@@ -12,12 +13,7 @@ def test_mongo_running_and_enabled(host):
     assert mongo.is_enabled
 
 # check if configuration file contains the required line
-def test_config_file(host):
-    config_file = host.file('/etc/mongod.conf')
+def test_config_file(File):
+    config_file = File('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
-
-# check listening ip mongo
-def test_http(host):
-    tcp = host.socket('tcp://0.0.0.0:27017')
-    assert tcp.is_listening
